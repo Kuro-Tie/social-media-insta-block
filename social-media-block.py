0231c062fa050235d9f89813aae2316bb5da230a
Kuro-Tie <gutstonirvana@gmail.com>
2025-12-01 16:26:35 +0300

    GUI with classes

--- social-media-block.py
+++ social-media-block.py
@@ -1,40 +1,34 @@
-import pyuac
-import sys # This module needs for admin code
+import tkinter as tk
+from tkinter import ttk
 
+class GUIApp(tk.Tk):
+    def __init__(self,mytitle,size):
+        super().__init__()
+        self.title(mytitle)
+        self.geometry(f'{size[0]}x{size[1]}')
+        self.minsize(size[0],size[1])
+        self.maxsize(size[0],size[1])
+        self.menu = Menu(self)
 
-class AdminTaskRunner:
-    """
-    #using pyuac admin role 
-    """
+        self.mainloop()
 
-    def __init__(self):
-        """Automatically checkj for admin"""
-        print("AdminTaskRunner starting...")
-        #We check user is admin or not
-        if not pyuac.isUserAdmin():
-            print("User is not admin! Restarting with admin rights!")
-            # runAsAdmin stops for admin rights and then restarts
-            # python sessien end
-            pyuac.runAsAdmin()
-            # if reboot okay it will not come here
-            sys.exit(0) 
-        else:        
-            print("This program running admin")
-            input("running as admin press enter to continue!")
+class Menu(ttk.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)    
+        self.place(x=0,y=0)
+        self.create_widgets()
+        self.create_layout()
 
-    def run_admin_operations(self):
-        
-        return
-
-
-# --- Basic usage ---
-if __name__ == "__main__":
-    
-    # 1. class.
-    # __init__ metod starts admin
-    # 
-    runner = AdminTaskRunner()
-    
-    # 2. Method for admin rights
-    # Only you can access admin rights
-    runner.run_admin_operations()
+    def create_widgets(self):
+        self.Label_text = ttk.Label(self, text="Instagram Blocker!",font=('Helvetica',18), background="orange")
+        self.Block_button = ttk.Button(self, text="Block")
+        self.UNBlock_button = ttk.Button(self, text="Unblock")
+        self.CodedByText = ttk.Label(self, text="Coded By Kuro-Tie",font=('Helvetica',18), background="purple")
+    def create_layout(self):
+        self.columnconfigure((0,1,2), weight = 1, uniform = 'a')
+        self.rowconfigure((0,1,2,3,4), weight = 1, uniform = 'a')
+        self.Label_text.grid(row=0,column=0,sticky='nswe',columnspan=4)
+        self.Block_button.grid(row=1,column=0,sticky='nswe',columnspan=2)
+        self.UNBlock_button.grid(row=1,column=2,sticky='nswe',columnspan=2)
+        self.CodedByText.grid(row=2,column=0,columnspan=4,sticky='nswe')
+GUIApp("Instagram Blocker", (210,120))
